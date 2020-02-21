e2a03cd13877665eae2fc51356d45ed4cc3304b5
Walter Scarborough <wscarborough@pivotal.io>
2020-02-21 16:04:03 -0500

    Fix postgres default password && allow postgres to start before running rake in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ docker-compose run web rails new . --force --no-deps --database=postgresql
 docker-compose build
 cp new_database_config.yml config/database.yml
 rm new_database_config.yml
-docker-compose run web rake db:create
+docker-compose up -d
 
+sleep 15
+
+docker-compose run web rake db:create
 
